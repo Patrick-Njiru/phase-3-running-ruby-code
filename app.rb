7f5b8ae7eb1b4a4e42c3afe15667d7a0749fddfa
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-10-17 09:44:09 +0300

    Completed assignment.

---
+++ app.rb
@@ -0,0 +1,11 @@
+puts 'Hello World!' # puts === console.log()
+puts 'Hello World!'
+puts 'Hello World!'
+
+print 'Hello World!' 
+print 'Hello World!' 
+print 'Hello World!' 
+puts
+
+p [1,2,3]
+pp [{ id: 1, Hello: 'World!'}, {id: 2, name: 'Patrick'}, {id: 3, language: "Ruby"}]
